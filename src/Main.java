02f36d1d7724698553d1bdde74e0dc336de3ecf6
Viktorryy <viktory_o@rambler.ru>
2022-09-30 23:39:07 +0400

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,44 @@
 public class Main {
 
-    // Задание 1
-    public static String leapYear(int yearL) {
-        int y4;
-        int y100;
-        int y400;
-        y4 = yearL % 4;
-        y100 = yearL % 100;
-        y400 = yearL % 400;
-        String year1 = "";
+    // Задание 2
+    public static String versionOs (byte sisCl, int yearCl) {
+        String message = "";
+        int clientDeviceYear = 2015;
 
-        if (y4 == 0 && y100 != 0 || y400 == 0) {
-            year1 = (yearL + " год является высокосным");
+        // операционная система клиента "iOS" = 0, "Android" = 1;
+        if (sisCl == 0) {
+            if (yearCl >= clientDeviceYear) {
+                message = ("Установите версию приложения для iOS по ссылке");
+            } else {
+                message = ("Установите облегченную версию приложения для iOS по ссылке");
+            }
         }
 
-        else {
-            year1 = (yearL + " год неявляется высокосным");
+        if (sisCl == 1) {
+            if (yearCl >= clientDeviceYear) {
+                message = ("Установите версию приложения для Android по ссылке");
+            } else {
+                message = ("Установите облегченную версию приложения для Android по ссылке");
+            }
         }
-
-        return (year1);
+        return message;
     }
 
     public static void main(String[] args) {
 
-        int year = 2030;
 
-        String yearStr = leapYear(year);
+        // возвращаемые значения
+        // считаем, что это оператор вернул значение ))
+        // операционная система клиента "iOS" = 0, "Android" = 1;
 
-        System.out.println(yearStr);
+        int clienYear = 2013;
+        byte clientOS = 1;
+        String ver = "";
 
-    }
+        ver = versionOs(clientOS, clienYear);
+        System.out.println(ver);
+
+        }
 
 
 }
